779ada6174f169da4308168154c3f3f2fd046fa6
Edwin Badillo <edwbadillo95@gmail.com>
2023-09-21 21:29:39 -0500

    add exists_by_id method

--- app/school/grade/repo.py
+++ app/school/grade/repo.py
@@ -7,5 +7,20 @@ from .models import Grade
 async def get_grades(db: AsyncSession) -> list[Grade]:
     """
     Lista todos los grados registrados definidos por el sistema educativo y del colegio.
+
+    Args:
+        db (AsyncSession): Sesión de base de datos
     """
-    return await db.scalars(select(Grade))
+    return list(await db.scalars(select(Grade)))
+
+
+async def exists_by_id(db: AsyncSession, id: int) -> bool:
+    """
+    Devuelve True si el grado con el id dado existe en la base de datos.
+
+    Args:
+        db (AsyncSession): Sesión de base de datos
+        id (int): ID del registro a verificar
+    """
+    stmt = select(Grade.id).where(Grade.id == id)
+    return (await db.scalar(stmt)) is not None
--- tests/school/grade/test_repo.py
+++ tests/school/grade/test_repo.py
@@ -1,16 +1,32 @@
+import pytest
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.school.grade.models import Grade
-from app.school.grade.repo import get_grades
+from app.school.grade.repo import exists_by_id, get_grades
 
 
 async def test_list_grades(db: AsyncSession, grades: list[Grade]):
     """Lista todos los grados del colegio"""
 
-    grades_list = list(await get_grades(db))
+    grades_list = await get_grades(db)
     assert len(grades_list) == len(grades)
 
     first_grade = grades_list[0]
     assert first_grade.id == grades[0].id
     assert first_grade.name == grades[0].name
     assert first_grade.level == grades[0].level
+
+
+@pytest.mark.parametrize(
+    "id, exists",
+    [
+        (0, True),
+        (345, False),
+    ],
+)
+async def test_exists_by_id(
+    db: AsyncSession, grades: list[Grade], id: int, exists: bool
+):
+    """Verifica que existe el grado con el id dado"""
+
+    assert await exists_by_id(db, id) == exists
