17c9c4a2f4aee930b9fbea1a3ff86f62fbdbbbbc
Edwin Badillo <edwbadillo95@gmail.com>
2023-09-23 15:26:39 -0500

    add common model fiels

--- app/common/database/models.py
+++ app/common/database/models.py
@@ -1,7 +1,30 @@
-from sqlalchemy.orm import DeclarativeBase
+from datetime import datetime
+from typing import Annotated
+
+from sqlalchemy import func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+# Tipo definido para un campo fecha que se especifica automáticamente por el motor
+AutoTimestamp = Annotated[
+    datetime, mapped_column(nullable=False, server_default=func.now())
+]
+IntPK = Annotated[int, mapped_column(primary_key=True)]
 
 
 class Base(DeclarativeBase):
     """Todo modelo ORM de SQLAlchemy debe extender esta clase."""
 
     pass
+
+
+class TimestampsMixin:
+    """
+    Mixin para agregar los campos created_at y updated_at que son establecidos
+    automáticamente por el motor de base de datos directamente.
+
+    Al usar PostgreSQL, es necesario ejecutar un script SQL para que el campo updated_at
+    se actualice correctamente al editar un registro.
+    """
+
+    created_at: Mapped[AutoTimestamp]
+    updated_at: Mapped[AutoTimestamp]
--- app/school/academicyear/models.py
+++ app/school/academicyear/models.py
@@ -3,28 +3,16 @@ from datetime import date
 from sqlalchemy import CheckConstraint
 from sqlalchemy.orm import Mapped, mapped_column
 
-from app.common.database.models import Base
+from app.common.database.models import Base, IntPK, TimestampsMixin
 
 
-class AcademicYear(Base):
+class AcademicYear(Base, TimestampsMixin):
     """
     Almacena los datos de un año escolar con el cual se hará referencia para las
     matrículas, periodos, cupos, etc.
     """
 
     __tablename__ = "academic_year"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-
-    # Año escolar
-    year: Mapped[int] = mapped_column(unique=True)
-
-    # Fecha de inicio de clases
-    start_date: Mapped[date]
-
-    # Fecha de fin de clases
-    end_date: Mapped[date]
-
     __table_args__ = (
         CheckConstraint("end_date > start_date", name="classes_dates"),
         CheckConstraint(
@@ -32,3 +20,8 @@ class AcademicYear(Base):
         ),
         CheckConstraint("DATE_PART('year', end_date) = year", name="year_in_end_date"),
     )
+
+    id: Mapped[IntPK]
+    year: Mapped[int] = mapped_column(unique=True)
+    start_date: Mapped[date]
+    end_date: Mapped[date]
