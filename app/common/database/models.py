dc2377f1ea0588181bb459423d88b548371aa866
Edwin Badillo <edwbadillo95@gmail.com>
2023-09-25 08:35:19 -0500

    updated timestamps defaults

--- app/common/database/models.py
+++ app/common/database/models.py
@@ -1,13 +1,10 @@
 from datetime import datetime
-from typing import Annotated
+from typing import Annotated, Optional
 
 from sqlalchemy import MetaData, func
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
-# Tipo definido para un campo fecha que se especifica automáticamente por el motor
-AutoTimestamp = Annotated[
-    datetime, mapped_column(nullable=False, server_default=func.now())
-]
+# Tipo para cualquier ID numérico
 IntPK = Annotated[int, mapped_column(primary_key=True)]
 
 
@@ -33,7 +30,12 @@ class TimestampsMixin:
 
     Al usar PostgreSQL, es necesario ejecutar un script SQL para que el campo updated_at
     se actualice correctamente al editar un registro.
+
+    Revise el archivo scripts/migrate.py para más información.
     """
 
-    created_at: Mapped[AutoTimestamp]
-    updated_at: Mapped[AutoTimestamp]
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+
+    # updated_at está establecido por el motor de base de datos cuando se actualiza un
+    # registro mediante un trigger.
+    updated_at: Mapped[Optional[datetime]]
