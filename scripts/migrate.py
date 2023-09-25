1c4fae406ae7326c1a4993568ae15e2b96e4faf7
Edwin Badillo <edwbadillo95@gmail.com>
2023-09-25 08:35:34 -0500

    update migrate scripts

--- scripts/migrate.py
+++ scripts/migrate.py
@@ -1,22 +1,82 @@
 import asyncio
 
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import AsyncConnection, create_async_engine
 
 from app.common.database.models import Base
 from app.config import settings
 
+# Importar todos los modelos SQLAlchemy
+from app.school.academicyear.models import AcademicYear  # noqa
+from app.school.grade.models import Grade  # noqa
+
+
+async def __create_auto_updated_at_function(conn: AsyncConnection) -> None:
+    """
+    Crea una función que actualiza el campo updated_at de la tabla.
+    """
+    sql = """
+        CREATE OR REPLACE FUNCTION update_updated_at()
+        RETURNS TRIGGER AS $$
+        BEGIN
+            NEW.updated_at = NOW();
+            RETURN NEW;
+        END;
+        $$ LANGUAGE plpgsql;
+    """
+    await conn.execute(text(sql))
+
+
+async def __create_base_functions(conn: AsyncConnection):
+    """
+    Crea funciones base de PostgreSQL requeridos para posteriores triggers
+    """
+    print("Creating database functions...")
+    await __create_auto_updated_at_function(conn)
+    print("Database functions created!")
+
+
+async def __set_updated_at_trigger(conn: AsyncConnection, model: type[Base]) -> None:
+    """
+    Crea un trigger que actualiza el campo updated_at de la tabla al editar un registro.
+    """
+    sql = f"""
+        CREATE OR REPLACE TRIGGER update_updated_at
+        BEFORE UPDATE ON {model.__tablename__}
+        FOR EACH ROW
+        EXECUTE PROCEDURE update_updated_at();
+    """
+    await conn.execute(text(sql))
+
+
+async def __create_triggers(conn: AsyncConnection):
+    """
+    Crea los triggers a diferentes tablas creadas en la base de datos
+    """
+    print("Creating triggers...")
+    await __set_updated_at_trigger(conn, AcademicYear)
+    print("Triggers created!")
+
+
+async def __create_tables(conn):
+    """Crea las tablas de la base de datos"""
+    print("Creating tables...")
+    await conn.run_sync(Base.metadata.create_all)
+    print("Tables created!")
+
 
 async def migrate():
-    from app.school.academicyear.models import AcademicYear  # noqa
-    from app.school.grade.models import Grade  # noqa
+    """
+    Crea la estructura de tablas de la base de datos, funciones y triggers necesarios
+    en la base de datos.
 
-    print("Creating tables...")
-
+    NOTA IMPORTANTE: Esto puede ser remplazado por el sistema de migraciones de alembic
+    """
     engine = create_async_engine(settings.DATABASE_URL)
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
-    print("Tables created!")
+        await __create_base_functions(conn)
+        await __create_tables(conn)
+        await __create_triggers(conn)
 
 
 if __name__ == "__main__":
