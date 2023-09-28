0284cdcdf36476627d3b96d8269b9394119c748c
Edwin Badillo <edwbadillo95@gmail.com>
2023-09-28 08:48:42 -0500

    add unique test case

--- tests/school/academicyear/test_router.py
+++ tests/school/academicyear/test_router.py
@@ -5,11 +5,19 @@ from httpx import AsyncClient
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.common.responses import CommonResponse, ResourceCreatedResponse
+from app.common.messages import FORM_ERROR
+from app.common.responses import (
+    CommonResponse,
+    FormErrorResponse,
+    ResourceCreatedResponse,
+)
 from app.school.academicyear import messages
 from app.school.academicyear.models import AcademicYear
 from app.school.academicyear.router import BASE_URL
+from tests.faker import faker_db_func
 from tests.utils import check_resource_not_found_response
+
+from .faker import fake_academic_year
 
 
 async def test_get_all(client: AsyncClient, academic_year):
@@ -62,6 +70,35 @@ async def test_create(client: AsyncClient, db: AsyncSession):
     ).model_dump()
 
     assert response.json() == expected_response
+
+
+async def test_create_with_year_already_exists(db: AsyncSession, client: AsyncClient):
+    current_year = date.today().year
+    await faker_db_func(
+        db,
+        fake_academic_year,
+        year=current_year,
+        start_date=date(current_year, 1, 1),
+        end_date=date(current_year, 12, 31),
+    )
+
+    form = {
+        "year": current_year,
+        "start_date": f"{current_year}-01-01",
+        "end_date": f"{current_year}-12-31",
+    }
+
+    response = await client.post(f"{BASE_URL}", json=form)
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+    assert (
+        response.json()
+        == FormErrorResponse(
+            message=FORM_ERROR,
+            errors=[
+                {"field": "year", "message": messages.YEAR_EXISTS},
+            ],
+        ).model_dump()
+    )
 
 
 async def test_update(client: AsyncClient, academic_year: AcademicYear):
